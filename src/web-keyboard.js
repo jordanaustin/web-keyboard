cb8d5da8c8a88506906204cdf22b77300f538b3c
Jordan Austin <jaustin@vernier.com>
2018-11-20 12:20:01 -0800

    Hide keyboard button when alwayOpen property is set

--- src/web-keyboard.js
+++ src/web-keyboard.js
@@ -201,6 +201,7 @@ class WebKeyboard extends LitElement {
                 fill: white;
                 border: none;
                 background: rgb(45,45,45);
+                display: ${this.alwaysOpen ? 'none' : 'flex'}
             }
             .wrapper {
                 z-index: 9990;
@@ -278,7 +279,7 @@ class WebKeyboard extends LitElement {
                 grid-column: 2 / span 2;
             }
         </style>
-        <button class="choose-keyboard" hidden="${this.alwaysOpen}">
+        <button class="choose-keyboard">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path d="M20 5H4c-1.1 0-1.99.9-1.99 2L2 17c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm-9 3h2v2h-2V8zm0 3h2v2h-2v-2zM8 8h2v2H8V8zm0 3h2v2H8v-2zm-1 2H5v-2h2v2zm0-3H5V8h2v2zm9 7H8v-2h8v2zm0-4h-2v-2h2v2zm0-3h-2V8h2v2zm3 3h-2v-2h2v2zm0-3h-2V8h2v2z"/>
                 <path d="M0 0h24v24H0zm0 0h24v24H0z" fill="none"/>
